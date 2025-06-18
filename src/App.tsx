cba229b85cf0f9a1997705224013d14860e46f3e
Petro <petrotymchuck@gmail.com>
2025-06-18 09:40:56 +0300

    add admin layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
 import './App.css'
-import {useGetAllCategoriesQuery} from "./services/apiCategory.ts";
+import {BrowserRouter as Router, Route, Routes} from "react-router";
+import Home from "./pages/Dashboard/Home.tsx";
+import AdminLayout from "./layout/AdminLayout.tsx";
+import NotFound from "./pages/OtherPage/NotFound.tsx";
 
 const App: React.FC = () => {
-    const {data: categories, isLoading} = useGetAllCategoriesQuery();
-
-    console.log(categories);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+    console.log("App rendered");
 
     return (
     <>
-        <h1 className={"text-3xl font-bold underline"}>Hello world</h1>
+        <Router>
+            <Routes>
+                {/*<Route index element={<UserLayout>}></Route>*/}
 
-        <table className="mt-5 w-full border border-gray-300 rounded-lg overflow-hidden">
-            <thead className="bg-gray-100">
-            <tr>
-                <th className="px-4 py-2 border-b border-gray-300 text-left">ID</th>
-                <th className="px-4 py-2 border-b border-gray-300 text-left">Name</th>
-                <th className="px-4 py-2 border-b border-gray-300 text-left">Slug</th>
-                <th className="px-4 py-2 border-b border-gray-300 text-left">Image</th>
-            </tr>
-            </thead>
-            <tbody>
-            {categories && categories.map(cat => (
-                <tr key={cat.id} className="even:bg-gray-50 hover:bg-blue-50 transition-colors">
-                    <td className="px-4 py-2 border-b border-gray-200">{cat.id}</td>
-                    <td className="px-4 py-2 border-b border-gray-200">{cat.name}</td>
-                    <td className="px-4 py-2 border-b border-gray-200">{cat.slug}</td>
-                    <td className="px-4 py-2 border-b border-gray-200">
-                        <img src={cat.image} alt={cat.name} className="w-12 h-12 object-cover rounded" />
-                    </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+                <Route path={"admin"} element={<AdminLayout />}>
+                    <Route path="home" element={<Home />}/>
+                </Route>
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "swiper/swiper-bundle.css";
+import "flatpickr/dist/flatpickr.css";
 import App from './App.tsx'
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {ThemeProvider} from "./context/ThemeContext.tsx";
+import {StrictMode} from "react";
+import {AppWrapper} from "./components/common/PageMeta.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <StrictMode>
+        <ThemeProvider>
+            <AppWrapper>
+                <Provider store={store}>
+                    <App />
+                </Provider>,
+            </AppWrapper>
+        </ThemeProvider>
+    </StrictMode>,
 )
